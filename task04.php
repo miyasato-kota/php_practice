f7db4b3d4163830521c1bae4a0fc6c6dc79e2231
miyasato-kota <m.k0ta0805@gmail.com>
2021-02-01 14:16:25 +0000

    task04_3

--- task04.php
+++ task04.php
@@ -3,11 +3,11 @@ $num1 = 4;
 $num2 = 23;
 $sum_num;
 $array_num = array(1, 3, 5 ,7, 9);
-$array_max = array(9, 12, 15 ,61, 53);
+$array_max = array(92, 12, 15 ,61, 53);
 
 
 // 関数実行
-echo problem1(252);
+echo problem1(2);
 echo problem2($num1, $num2);
 echo problem3($array_num);
 echo max_array($array_max);
@@ -15,21 +15,25 @@ echo problem5($array_max, $array_num);
 
 // 問１
 function problem1($a) {
-    $a *= 2;
-    return $a. strip_tags(nl2br("\n"));
+    $b = $a * 2;
+    return $b;
 }
 // 問２
 function problem2($a, $b) {
     $sum_num = $a + $b;
-    return $sum_num. strip_tags(nl2br("\n"));
+    return $sum_num;
 }
 // 問3
 function problem3($arr) {
-    $get_array_num = 1;
-    foreach ($arr AS $value) {
-        $get_array_num *= $value;
+    $arr_result = 0;
+    for ($i = 0; $i < count($arr); $i++) {
+        if($i == 0){
+            $arr_result += $arr[$i];
+        }else{
+            $arr_result *= $arr[$i];
+        }
     }
-    return $get_array_num. strip_tags(nl2br("\n"));
+    return $arr_result.'<br>';
 }
 // 問4
 function max_array($arr) {
@@ -39,19 +43,19 @@ function max_array($arr) {
             $max_number = $a; 
          }
      }
-     return $max_number. strip_tags(nl2br("\n")); 
+     return $max_number; 
 }
 // 問5
 function problem5($arr, $arr1) {
     
     // 1.
     $str = "<p><?php var_dump('string'); ?>テスト<br>テキスト</p>";
-    $result = strip_tags($str). strip_tags(nl2br("\n"));
+    $result = strip_tags($str);
 
     // 2.
     array_push($arr, 20, 35, 46, 3, 999);
     foreach ($arr AS $merge) {
-        $result .= $merge. strip_tags(nl2br("\n"));
+        $result .= $merge;
     }
 
     // 3.
@@ -60,14 +64,15 @@ function problem5($arr, $arr1) {
     $make_array = array_merge($array_string, $num_array);
     
     foreach ($make_array AS $arr_val) {
-        $result .= $arr_val. strip_tags(nl2br("\n"));
+        $result .= $arr_val;
     }
     
     // 4.
-    $nuw_time = time();
-    $get_tiem = mktime(1);
-    $result .= $nuw_time. strip_tags(nl2br("\n"));
-    $result .= $get_tiem. strip_tags(nl2br("\n"));
+    $time = time() + (7 * 24 * 60 * 60);
+    $nuw_time = date('Y-m-d', $time);
+    $get_tiem = date("M-d-Y", mktime(0, 0, 0, 1, 1, 2021));;
+    $result .= $nuw_time;
+    $result .= $get_tiem;
     
     // 5.
     $get_date = date('Y-m-d H:i:s');
